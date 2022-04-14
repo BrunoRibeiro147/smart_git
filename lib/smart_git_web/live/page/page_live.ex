34e4de361402323e19c75fd3bb28ef5e2498eada
Bruno <brf147@gmail.com>
2022-04-14 16:05:17 -0400

    feat: Finish first class

--- lib/smart_git_web/live/page/page_live.ex
+++ lib/smart_git_web/live/page/page_live.ex
@@ -1,5 +1,10 @@
 defmodule SmartGitWeb.PageLive do
+  @moduledoc """
+  Controller for PageLive
+  """
+
   use SmartGitWeb, :live_view
+  alias SmartGitWeb.Page.RepoDetail
 
   def mount(_params, _session, socket) do
     {:ok, socket}
--- test/smart_git_web/live/page/page_live_test.exs
+++ test/smart_git_web/live/page/page_live_test.exs
@@ -3,8 +3,12 @@ defmodule SmartGitWeb.PageLiveTest do
   import Phoenix.LiveViewTest
 
   test "load page elements", %{conn: conn} do
-    {:ok, view, html} = live(conn, Routes.page_path(conn, :index))
+    {:ok, view, _html} = live(conn, Routes.page_path(conn, :index))
 
     assert has_element?(view, "[data-role=btn-language-select][data-id=elixir]", "Elixir")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=javascript]", "Javascript")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=java]", "Java")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=python]", "Python")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=go]", "Go")
   end
 end
